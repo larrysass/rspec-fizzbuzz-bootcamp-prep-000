2a6f878e5d650b1f223c0e8fa4e63a9614e9f964
Larry Sass-Ainsworth <larry.sassainsworth@gmail.com>
2019-05-21 15:58:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,12 @@
 
 
 def fizzbuzz(n)
-  if n % 3 == 0 
-    "Fizz"
+  if(n % 5 == 0) && (n % 3 == 0)
+    "FizzBuzz"
   elsif n % 5 == 0 
     "Buzz"
-  elsif (n % 5 == 0) && (n % 3 == 0)
-    "FizzBuzz"
+  elsif (n % 3 == 0)
+    "Fizz"
   end
   
 end 
